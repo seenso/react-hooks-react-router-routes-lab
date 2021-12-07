8b78dc35873941124dc87e98752eafc4d1a21f5e
seenso <>
2021-12-06 16:09:16 -0900

    Rendered App, NavBar, Home components.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,22 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>{/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map(m => {
+        return (<div key={m.title}>
+          Title: {m.title} Time: {m.time} <br />
+          <ul>
+            {m.genres.map(genre => {
+              return (<li>{genre}</li>);
+            })}
+          </ul>
+        </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Movies;
